a9829fd5f20544c3ada6c1045e973640747a3173
Muhammad Luthfi Khusyasy <khusyasy@gmail.com>
2021-06-11 21:53:52 +0700

    install typed.js create folders (js, css) create animation

--- src/index.js
+++
@@ -1,1 +0,0 @@
-document.getElementById("root").style.background = "#000";
---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import Typed from "typed.js";
+
+var typed = new Typed("#typing-box", {
+    stringsElement: "#typing-text",
+    typeSpeed: 20,
+    startDelay: 500,
+    backSpeed: 20,
+});
