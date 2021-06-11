baae947665102de82ee2fa8fc637813cec9b3deb
Muhammad Luthfi Khusyasy <khusyasy@gmail.com>
2021-06-12 06:14:33 +0700

    config aos refactor div jumbo

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,25 @@
 import Typed from "typed.js";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 var typed = new Typed("#typing-box", {
-    stringsElement: "#typing-text",
-    typeSpeed: 50,
-    startDelay: 200,
-    backSpeed: 30,
-    backDelay: 1000,
+  stringsElement: "#typing-text",
+  typeSpeed: 50,
+  startDelay: 1400,
+  backSpeed: 30,
+  backDelay: 1000,
+});
+
+// AOS.init();
+AOS.init({
+  disable: false,
+  startEvent: "DOMContentLoaded",
+  useClassNames: false,
+  disableMutationObserver: false,
+  debounceDelay: 50,
+  throttleDelay: 99,
+
+  easing: "ease-in-out",
+  once: false,
+  mirror: true,
 });
