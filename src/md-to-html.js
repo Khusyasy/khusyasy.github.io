4ec2948413ea171a1f642856fe5d95e137d93b39
Muhammad Luthfi Khusyasy <khusyasy@gmail.com>
2021-06-14 18:45:54 +0700

    create projects card tempalate and render

--- src/md-to-html.js
+++ src/md-to-html.js
@@ -7,6 +7,8 @@ const folder = "projects";
 
 const layout = fs.readFileSync(path.join(src_path, folder, "layout.html"), 'utf8');
 
+let projects_file = [];
+
 fs.readdirSync(path.join(src_path, folder)).forEach(file => {
   let [name, ext] = file.split(".");
   if(ext !== "md") return;
@@ -25,9 +27,27 @@ fs.readdirSync(path.join(src_path, folder)).forEach(file => {
     }
     html = html.replace(RegExp(`{[ ]*content[ ]*}`), marked(content));
 
-    const write = fs.writeFileSync(path.join(src_path, folder, file_to_create), html);
+    fs.writeFileSync(path.join(src_path, folder, file_to_create), html);
 
+    projects_file.push({file: file_to_create, ...gm.data});
+    
   } catch (err) {
     console.error(err)
   }
 });
+
+const card_layout = fs.readFileSync(path.join(src_path, "project-card.html"), 'utf8');
+let cards = "";
+
+projects_file.forEach(project => {
+  let card = card_layout;
+  for(d of Object.entries(project)){
+    card = card.replace(RegExp(`{[ ]*${d[0]}[ ]*}`, "g"), d[1]);
+  }
+  cards += card + "\n";
+});
+
+let index = fs.readFileSync(path.join(src_path, "index.src.html"), 'utf8');
+index = index.replace(RegExp(`{[ ]*project-card[ ]*}`, "g"), cards);
+
+fs.writeFileSync(path.join(src_path, "index.html"), index);
