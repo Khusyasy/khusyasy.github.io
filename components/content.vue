858639d3f06c2149bd50a2cb07d359e9ecb29808
Luthfi Khusyasy <44360105+Khusyasy@users.noreply.github.com>
2023-07-18 19:55:55 +0700

    experience? try

--- components/content.vue
+++ components/content.vue
@@ -2,59 +2,62 @@
   <div class="wrapper" id="content-scroll">
     <div class="section" id="section-about">
       <h2 class="section-title">About</h2>
-      <p class="section-text">
-        I am a tech enthusiast with a strong passion for website development. With a vocational education in
-        <span class="text-highlight">Software Engineering</span>
-        and currently pursuing a degree in
-        <span class="text-highlight">Informatics</span>,
-        I have established a solid foundation in the
-        field.
-      </p>
+      <div class="section-content">
+        <p>
+          I am a tech enthusiast with a strong passion for website development. With a vocational education in
+          <span class="text-highlight">Software Engineering</span>
+          and currently pursuing a degree in
+          <span class="text-highlight">Informatics</span>,
+          I have established a solid foundation in the
+          field.
+        </p>
 
-      <p class="section-text">
-        Specializing in
-        <span class="text-highlight">Frontend Development</span>,
-        I acquired practical experience and developed my
-        expertise using
-        <span class="text-highlight">JavaScript</span>,
-        <span class="text-highlight">Vue.js</span>, and
-        <span class="text-highlight">React.js</span>.
-      </p>
+        <p>
+          Specializing in
+          <span class="text-highlight">Frontend Development</span>,
+          I acquired practical experience and developed my
+          expertise using
+          <span class="text-highlight">JavaScript</span>,
+          <span class="text-highlight">Vue.js</span>, and
+          <span class="text-highlight">React.js</span>.
+        </p>
 
-      <p class="section-text">
-        I am flexible and open to new opportunities to collaborate with others and create impactful digital solutions.
-      </p>
+        <p>
+          I am flexible and open to new opportunities to collaborate with others and create impactful digital solutions.
+        </p>
+      </div>
     </div>
     <div class="section" id="section-experience">
       <h2 class="section-title">Experience</h2>
-      <p class="section-text">
-        I am a tech enthusiast with a strong passion for website development. With a vocational education in
-        <span class="text-highlight">Software Engineering</span>
-        and currently pursuing a degree in
-        <span class="text-highlight">Informatics</span>,
-        I have established a solid foundation in the
-        field.
-      </p>
-
-      <p class="section-text">
-        Specializing in
-        <span class="text-highlight">Frontend Development</span>,
-        I acquired practical experience and developed my
-        expertise using
-        <span class="text-highlight">JavaScript</span>,
-        <span class="text-highlight">Vue.js</span>, and
-        <span class="text-highlight">React.js</span>.
-      </p>
-
-      <p class="section-text">
-        I am flexible and open to new opportunities to collaborate with others and create impactful digital solutions.
-      </p>
+      <div class="section-content">
+        <ExpItem v-for="props in experiences" :date="props.date" :content="props.content" :tags="props.tags" />
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-
+const experiences: Array<{
+  date: string
+  content: string
+  tags: string[]
+}> = [
+    {
+      date: 'Jan 2021 - Dec 2022',
+      content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.Officia illo nisi alias ipsam quia necessitatibus quaerat labore ipsum tenetur quasi, neque, suscipit numquam nam! Asperiores.',
+      tags: ['Vue.js', 'React.js', 'JavaScript']
+    },
+    {
+      date: 'Jan 2021 - Dec 2022',
+      content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.Officia illo nisi alias ipsam quia necessitatibus quaerat labore ipsum tenetur quasi, neque, suscipit numquam nam! Asperiores.',
+      tags: ['Vue.js', 'React.js', 'JavaScript']
+    },
+    {
+      date: 'Jan 2021 - Dec 2022',
+      content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.Officia illo nisi alias ipsam quia necessitatibus quaerat labore ipsum tenetur quasi, neque, suscipit numquam nam! Asperiores.',
+      tags: ['Vue.js', 'React.js', 'JavaScript']
+    },
+  ]
 </script>
 
 <style lang="scss" scoped>
@@ -85,8 +88,11 @@
   line-height: 1;
 }
 
-.section-text {
+.section-content {
   font-size: 1.25rem;
   text-align: justify;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 }
 </style>
---
+++ components/expItem.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="experience">
+    <h3>
+      {{ date }}
+    </h3>
+    <div class="exp-content">
+      <p>
+        {{ content }}
+      </p>
+      <div class="tag-container">
+        <div class="tag" v-for="tag in tags" :key="tag">{{ tag }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  date: string
+  content: string
+  tags: string[]
+}>()
+</script>
+
+<style lang="scss" scoped>
+.experience {
+  display: flex;
+  flex-direction: row;
+  gap: 0.8em;
+  font-size: 1rem;
+}
+
+h3 {
+  color: $primary-2;
+  font-weight: 400;
+  width: 50%;
+  opacity: 0.8;
+  font-size: 0.8rem;
+}
+
+.exp-content {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5em;
+}
+
+.tag-container {
+  display: flex;
+  flex-direction: row;
+  gap: 0.25em;
+}
+
+.tag {
+  background-color: $primary-1;
+  color: $black;
+  padding: 0.1em 0.25em;
+  border-radius: 2em;
+  font-size: 0.8rem;
+  font-weight: 400;
+}
+</style>
