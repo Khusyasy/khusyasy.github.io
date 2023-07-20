5d04aa7c54e22abf903d7d0787c509343eec6b4c
Luthfi Khusyasy <44360105+Khusyasy@users.noreply.github.com>
2023-07-20 09:56:29 +0700

    nav item style

--- components/navItem.vue
+++ components/navItem.vue
@@ -33,36 +33,38 @@ function handleClick(target: string) {
   opacity: 0.5;
   transition: all 0.2s ease-in-out;
   padding: 0;
-}
 
-.navitem:hover {
-  padding: 0 0.1rem;
-}
+  &::after {
+    content: "";
+    background-color: $white;
+    display: block;
+    width: 2rem;
+    height: 4px;
+    border-radius: 4px;
+    position: relative;
+    transition: width 0.2s ease-in-out;
+  }
 
-.navitem.active {
-  color: $primary-1;
-  opacity: 1;
-  padding: 0 0.25rem;
-}
+  &:hover {
+    padding: 0 0.1rem;
+    opacity: 1;
 
-.navitem::after {
-  content: "";
-  background-color: $white;
-  display: block;
-  width: 2rem;
-  height: 6px;
-  border-radius: 6px;
-  position: relative;
-  transition: width 0.2s ease-in-out;
-}
+    &::after {
+      width: max(4rem, 50%);
+      opacity: 1;
+    }
+  }
 
-.navitem:hover::after {
-  width: max(4rem, 50%);
-  opacity: 0.75;
-}
 
-.navitem.active::after {
-  width: 100% !important;
-  background-color: $primary-2;
+  &.active {
+    color: $primary-1;
+    opacity: 1;
+    padding: 0 0.25rem;
+
+    &::after {
+      width: 100% !important;
+      background-color: $primary-2;
+    }
+  }
 }
 </style>
